a0b90eeb769ad68bbf60d53341bf1611a56eda06
Diony Rosa <dhrosa@gmail.com>
2023-01-02 23:57:10 -0500

    Simply Event atomics.

--- src/event.h
+++ src/event.h
@@ -17,13 +17,9 @@ class Event {
   auto operator co_await();
 
  private:
-  // `state_' contains either a waiting coroutine address, or one of the special
-  // values below.
-  enum : std::uintptr_t {
-    kCleared = 1,
-    kSet = 2,
-  };
-  std::atomic_uintptr_t state_;
+  enum : int { kCleared, kWaiting, kSet };
+  std::atomic_int state_;
+  std::coroutine_handle<> waiting_;
 };
 
 inline Event::Event() { state_.store(kCleared, std::memory_order::relaxed); }
@@ -35,30 +31,27 @@ inline void Event::Notify() {
   if (previous_state == kCleared) {
     return;
   }
-  // Resume the waiter.
-  std::coroutine_handle<>::from_address(reinterpret_cast<void*>(previous_state))
-      .resume();
+  assert(previous_state == kWaiting);
+  waiting_.resume();
 }
 
 inline auto Event::operator co_await() {
   struct Waiter {
-    std::atomic_uintptr_t& state;
+    Event& event;
 
-    // Cached value of th load in await_ready() to use inside await_suspend().
-    std::uintptr_t previous_state;
+    // Cached value of the load in await_ready() to use inside await_suspend().
+    int previous_state;
 
     bool await_ready() {
-      previous_state = state.load(std::memory_order::acquire);
+      previous_state = event.state_.load(std::memory_order::acquire);
       return previous_state == kSet;
     }
 
     bool await_suspend(std::coroutine_handle<> handle) {
       assert(previous_state == kCleared);
-      if (state.compare_exchange_strong(
-              previous_state,
-              reinterpret_cast<std::uintptr_t>(handle.address()),
-              std::memory_order::acq_rel)) {
-        // kCleared -> waiting transition.
+      event.waiting_ = handle;
+      if (event.state_.compare_exchange_strong(previous_state, kWaiting,
+                                               std::memory_order::acq_rel)) {
         return true;
       }
       // Racing kCleared -> kSet transition. We should resume ourselves.
@@ -69,5 +62,5 @@ inline auto Event::operator co_await() {
     void await_resume() {}
   };
 
-  return Waiter{state_};
+  return Waiter{*this};
 }
